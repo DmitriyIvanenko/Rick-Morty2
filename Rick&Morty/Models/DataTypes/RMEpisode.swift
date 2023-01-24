b86369ce99ee2489a84239ed60bb630e3c16cc54
Dmytro Ivanenko <ivanenko2010@gmail.com>
2023-01-24 17:37:47 +0300

    BUG FIX - Details cell view (Character)
    Mistake in model for API - one letter

--- Rick&Morty/Models/DataTypes/RMEpisode.swift
+++ Rick&Morty/Models/DataTypes/RMEpisode.swift
@@ -13,7 +13,7 @@ struct RMEpisode: Codable, RMEpisodeDataRender {
     let name: String
     let air_date: String
     let episode: String
-    let character: [String]
+    let characters: [String]
     let url: String
     let created: String
     
